83f1ba489da0c03a058ba197638f3e20f386cf21
mourat <arakastanos@gmail.com>
2020-11-14 23:43:48 +0200

    Project Initialization with Spring Initialzr

---
+++ healthREST/src/test/java/com/pm/healthREST/HealthRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.healthREST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
