f0e0595ace09fe812f6ac662ca85feedb2281265
mourat <arakastanos@gmail.com>
2020-11-15 21:04:57 +0200

    Patient's DAO (Interface+ Implementation) created for retrieving object from db

---
+++ healthREST/src/main/java/com/pm/healthREST/dao/PatientDAO.java
@@ -0,0 +1,9 @@
+package com.pm.healthREST.dao;
+
+import com.pm.healthREST.entity.Patient;
+
+public interface PatientDAO {
+	
+	public Patient getPatient(int id);
+
+}
---
+++ healthREST/src/main/java/com/pm/healthREST/dao/PatientDAOImpl.java
@@ -0,0 +1,38 @@
+package com.pm.healthREST.dao;
+
+import javax.persistence.EntityManager;
+
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.pm.healthREST.entity.Patient;
+
+@Repository
+public class PatientDAOImpl implements PatientDAO {
+
+	
+	private EntityManager entityManager;
+	
+	
+	//constructor injection
+	@Autowired
+	public PatientDAOImpl(EntityManager theEntityManager) {
+		entityManager = theEntityManager;
+		
+	}
+	
+	
+	
+	@Override
+	@Transactional
+	public Patient getPatient(int id) {
+		
+		Session currentSession = entityManager.unwrap(Session.class);
+		Patient thePatient = currentSession.get(Patient.class, id);
+		
+	return thePatient;
+	}
+
+}
