d66077e417282a4f06ff3de59d157130b42076af
mourat <arakastanos@gmail.com>
2020-11-21 23:49:08 +0200

    Added rest controller for regisauthentication with jwt with free endpoints

---
+++ healthREST/src/main/java/com/pm/healthREST/rest/PatientRestSecuredController.java
@@ -0,0 +1,69 @@
+package com.pm.healthREST.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pm.healthREST.dto.PatientDTO;
+import com.pm.healthREST.jwt.JwtRequest;
+import com.pm.healthREST.jwt.JwtResponse;
+import com.pm.healthREST.jwt.JwtTokenUtil;
+import com.pm.healthREST.service.PatientUserDetailsService;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/auth")
+public class PatientRestSecuredController {
+	
+	
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private JwtTokenUtil jwtTokenUtil;
+
+	@Autowired
+	private PatientUserDetailsService userDetailsService;
+
+	@RequestMapping(value = "/patient", method = RequestMethod.POST)
+	public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
+
+		authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+
+		final UserDetails userDetails = userDetailsService
+				.loadUserByUsername(authenticationRequest.getUsername());
+
+		final String token = jwtTokenUtil.generateToken(userDetails);
+
+		return ResponseEntity.ok(new JwtResponse(token));
+	}
+	
+	@RequestMapping(value = "/patients", method = RequestMethod.POST)
+	public ResponseEntity<?> saveUser(@RequestBody PatientDTO user) throws Exception {
+		return ResponseEntity.ok(userDetailsService.save(user));
+	}
+
+	private void authenticate(String username, String password) throws Exception {
+		try {
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+		} catch (DisabledException e) {
+			throw new Exception("USER_DISABLED", e);
+		} catch (BadCredentialsException e) {
+			throw new Exception("INVALID_CREDENTIALS", e);
+		}
+	}
+}
+
+	
+
+
