f7251ea642ea180e3c1f590bee20a638c70f5e5a
mourat <arakastanos@gmail.com>
2020-11-21 23:02:12 +0200

    Created Patient DTO and Patient Service for authentication reasons

---
+++ healthREST/src/main/java/com/pm/healthREST/dto/PatientDTO.java
@@ -0,0 +1,51 @@
+package com.pm.healthREST.dto;
+
+public class PatientDTO {
+	private String username;
+	private String password;
+	private String email;
+	private String firstname;
+	private String lastname;
+	
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getLastName() {
+		return lastname;
+	}
+
+	public void setLastName(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFirstName() {
+		return firstname;
+	}
+
+	public void setFirstName(String firstname) {
+		this.firstname = firstname;
+	}
+}
+
---
+++ healthREST/src/main/java/com/pm/healthREST/service/PatientUserDetailsService.java
@@ -0,0 +1,44 @@
+package com.pm.healthREST.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+import com.pm.healthREST.dao.PatientDAO;
+import com.pm.healthREST.dto.PatientDTO;
+import com.pm.healthREST.entity.Patient;
+
+@Service
+public class PatientUserDetailsService implements UserDetailsService {
+	@Autowired
+	private PatientDAO userDao;
+
+	@Autowired
+	private PasswordEncoder bcryptEncoder;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Patient user = userDao.findByUsername(username);
+		if (user == null) {
+			throw new UsernameNotFoundException("User not found with username: " + username);
+		}
+		return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+				new ArrayList<>());
+	}
+
+	public Patient save(PatientDTO user) {
+		Patient newUser = new Patient();
+		newUser.setUsername(user.getUsername());
+		newUser.setPassword(bcryptEncoder.encode(user.getPassword()));
+		newUser.setFirstName(user.getFirstName());
+		newUser.setLastName(user.getLastName());
+		newUser.setEmail(user.getEmail());
+		return userDao.save(newUser);
+	}
+}
+
