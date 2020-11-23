3d75cdea30285b78ed0895ef785dc39a03b36940
mourat <arakastanos@gmail.com>
2020-11-23 04:24:29 +0200

    Updated our Controller with new (secured) endpoints

--- healthREST/src/main/java/com/pm/healthREST/rest/PatientRestController.java
+++ healthREST/src/main/java/com/pm/healthREST/rest/PatientRestController.java
@@ -7,13 +7,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pm.healthREST.dao.FeverSessionDAO;
 import com.pm.healthREST.dao.PatientDAO;
+import com.pm.healthREST.dao.TemperatureDAO;
 import com.pm.healthREST.entity.Patient;
+import com.pm.healthREST.entity.Temperature;
+import com.pm.healthREST.service.PlayerService;
 
 @RestController
 @RequestMapping("/api")
@@ -24,16 +29,23 @@ public class PatientRestController {
 	private PasswordEncoder bcryptEncoder;
 	
 	@Autowired
+	PlayerService playerService;
+	
+	@Autowired
 	private PatientDAO patientDAO;
 	
-
-
+	//@Autowired
+	//private TemperatureDAO temperatureDAO;
+	
+	//@Autowired
+	//private FeverSessionDAO feverSessionDAO;
+	
 	@Autowired
-	public PatientRestController(PatientDAO thePatientDAO ) {
+	public PatientRestController(PatientDAO thePatientDAO, TemperatureDAO theTemperatureDAO,FeverSessionDAO theFeverSessionDAO ) {
 		
 		patientDAO = thePatientDAO;
-		
-		
+		//temperatureDAO = theTemperatureDAO;
+	//	feverSessionDAO = theFeverSessionDAO;
 	}
 	
 	@GetMapping("/patients/{id}")
@@ -56,33 +68,22 @@ public class PatientRestController {
 	 
 	 }
 
-		
-		
-	
-	
-	
-	
-	
-	
-	/*
-	
-	@PostMapping("/patients")
-	public Patient registerPatient(@RequestBody Patient thePatient) {
-		
-		//set id to 0 for update execution (for hibernate saveorupdate)
-		thePatient.setId(0);
-		patientDAO.save(thePatient);
-		System.out.println(thePatient);
-		return thePatient;
-	}
-	
+	@GetMapping("/patients/health/{id}")
+	public String getHealth(@PathVariable int id, Principal principal, Patient thePatient ) throws Exception {
 	
 	Optional<Patient> patient = patientDAO.findById(id);
 	 // Check if patient access his own data or not
 	 String myPatientsName = patient.get().getUsername();
 	 String x =principal.getName();
 	 if (x.equals(myPatientsName)) {
-		 return patient;
+		 
+		 if(patient.get().getFlag()) {
+			 return "ONGOING FEVER";
+		 }
+		 else {
+			 return "HEALTHY";
+		 }
+			 
 
 	 }
 	
@@ -91,11 +92,16 @@ public class PatientRestController {
  throw new Exception("Not current users data");
 	 
 	}
-	
-	*/
+	 
+	 }
+		
+
 	
 	@PutMapping("/patients/{id}")
 	public Patient updatePatient(@RequestBody Patient thePatient, @PathVariable int id, Principal principal) throws Exception {
+			
+		
+		
 		Optional<Patient> patient = patientDAO.findById(id);
 		 // Check if patient access his own data or not
 		 String myPatientsName = patient.get().getUsername();
@@ -117,6 +123,15 @@ public class PatientRestController {
 	 
 	}
 		 
+	}
 	
-	}	
+	@PostMapping("/patients/{username}")
+	public Temperature saveTemperature(@RequestBody Temperature theTemperature, @PathVariable String username, Principal principal) throws Exception {
+		
+		return playerService.saveTemperature(theTemperature, username, principal);
+	 
+	}
+	
+	
+	
 }
